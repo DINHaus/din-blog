2e6d7dfee340ea0af05097bb48d56c021b2d106b
Dekan <dekanbrown@odyssy.io>
2024-05-24 08:20:09 -0600

    add single file build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   define: {
     "process.env": {
       VITE_GRAPH_API_KEY_MAINNET: JSON.stringify(process.env.VITE_GRAPH_API_KEY_MAINNET),
